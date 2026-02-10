272af35b55cad810092d2ab9078e88f0ea95d9a3
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-10 21:54:53 +0530

    [Jay]: refactor(repository): update CategoryRepository

--- src/main/java/com/gevernova/Task_management_system/repository/CategoryRepository.java
+++ src/main/java/com/gevernova/Task_management_system/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
-package com.gevernova.task_management_system.repository;
+package com.gevernova.Task_management_system.repository;
 
-import com.gevernova.task_management_system.model.Category;
+import com.gevernova.Task_management_system.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {}
