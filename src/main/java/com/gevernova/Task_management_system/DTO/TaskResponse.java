aa61d05894ee7ed4aff7a96d1290307faf9b2bce
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-10 21:45:06 +0530

    refactor(dto): restructure TaskResponse with nested DTOs

--- src/main/java/com/gevernova/Task_management_system/DTO/TaskResponse.java
+++ src/main/java/com/gevernova/Task_management_system/DTO/TaskResponse.java
@@ -1,4 +1,15 @@
 package com.gevernova.Task_management_system.DTO;
 
+import lombok.Data;
+
+@Data
 public class TaskResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+
+    private UserDTO user;
+    private CategoryDTO category;
+    private PriorityDTO priority;
 }
