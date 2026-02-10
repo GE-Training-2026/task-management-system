535bb9bf04efbeb7f6b4dd46a5c75f1420c9b368
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-10 21:46:09 +0530

    [Jay]: refactor(dto): update UserDTO response model

--- src/main/java/com/gevernova/Task_management_system/DTO/UserDTO.java
+++ src/main/java/com/gevernova/Task_management_system/DTO/UserDTO.java
@@ -1,4 +1,10 @@
 package com.gevernova.Task_management_system.DTO;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
 }
