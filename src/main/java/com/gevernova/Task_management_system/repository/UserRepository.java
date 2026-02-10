3e93d31516517cb7ae863f30be12d44105dbb63d
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-10 21:56:39 +0530

    [Jay]: refactor(repository): update UserRepository

--- src/main/java/com/gevernova/Task_management_system/repository/UserRepository.java
+++ src/main/java/com/gevernova/Task_management_system/repository/UserRepository.java
@@ -1,5 +1,5 @@
-package com.gevernova.task_management_system.repository;
-import com.gevernova.task_management_system.model.User;
+package com.gevernova.Task_management_system.repository;
+import com.gevernova.Task_management_system.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
