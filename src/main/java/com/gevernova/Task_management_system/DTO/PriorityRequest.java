50bb2a87b238e355b64720bbacc4ee4c948fdc38
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-10 21:44:11 +0530

    [Jay]: refactor(dto): update PriorityRequest payload

--- src/main/java/com/gevernova/Task_management_system/DTO/PriorityRequest.java
+++ src/main/java/com/gevernova/Task_management_system/DTO/PriorityRequest.java
@@ -1,4 +1,7 @@
 package com.gevernova.Task_management_system.DTO;
+import lombok.Data;
 
+@Data
 public class PriorityRequest {
+    private String level; // HIGH, MEDIUM, LOW (if you want enum later)
 }
